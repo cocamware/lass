61613dc899e9da173f16b6e81c1050199ed34292
Bram de Greve <bram.degreve@bramz.net>
2004-09-13 16:07:47 +0000

    porting param_expander from perl to python
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/trunk@11 e9412239-ebb7-49aa-8f4a-f9aa784616d7

---
+++ src/test/pre_build.py
@@ -0,0 +1,6 @@
+execfile(r'..\..\tools\param_expander.py')
+
+print 'Code generation ...'
+
+expandFile('test_util_callback.tmpl.inl', 'test_util_callback.inl', 3)
+
---
+++ src/util/pre_build.py
@@ -0,0 +1,17 @@
+execfile(r'..\..\tools\param_expander.py')
+
+print 'Code generation ...'
+
+expandFile('object_factory.tmpl.h', 'object_factory.h', 10)
+expandFile('clone_factory.tmpl.h', 'clone_factory.h', 10)
+
+expandFile('callback_x.tmpl.h', 'callback_$x.h', 10)
+expandFile('callback_r_x.tmpl.h', 'callback_r_$x.h', 10)
+expandFile('impl\dispatcher_x.tmpl.h', 'impl\dispatcher_$x.h', 10)
+expandFile('impl\dispatcher_r_x.tmpl.h', 'impl\dispatcher_r_$x.h', 10)
+
+expandFile('pyobject_macros.tmpl.h', 'pyobject_macros.h', 10)
+expandFile('pyobject_call.tmpl.inl', 'pyobject_call.inl', 10)
+
+expandFile('thread_fun.tmpl.h', 'thread_fun.h', 10)
+expandFile('thread_fun.tmpl.inl', 'thread_fun.inl', 10)
