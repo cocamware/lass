5f20885abb478147df32090bfddb2f91113d6532
Bram de Greve <bram.degreve@bramz.net>
2007-05-06 21:17:25 +0000

    suppress warning for multiple assignment operators too
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/trunk@628 e9412239-ebb7-49aa-8f4a-f9aa784616d7

--- src/util/atomic.h
+++ src/util/atomic.h
@@ -112,6 +112,7 @@ void atomicDecrement(volatile T& value)
 #if LASS_COMPILER_TYPE == LASS_COMPILER_TYPE_MSVC
 #	pragma warning(push)
 #	pragma warning(disable: 4521) // multiple copy constructors specified
+#	pragma warning(disable: 4522) // multiple assignment operators specified
 #endif
 
 /** Pointer with a tag for ABA salvation
