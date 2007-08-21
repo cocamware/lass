c891d3602473f3adc13f16fb51e8fe8d82750141
Bram de Greve <bram.degreve@bramz.net>
2007-08-21 19:14:20 +0000

    relicensing LASS under CPAL/GPL dual license.
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/trunk@679 e9412239-ebb7-49aa-8f4a-f9aa784616d7

--- src/stde/overwrite_insert_iterator.h
+++ src/stde/overwrite_insert_iterator.h
@@ -1,26 +1,43 @@
-/** @file
- *  @author Bram de Greve (bramz@users.sourceforge.net)
- *  @author Tom De Muer (tomdemuer@users.sourceforge.net)
+/**	@file
+ *	@author Bram de Greve (bramz@users.sourceforge.net)
+ *	@author Tom De Muer (tomdemuer@users.sourceforge.net)
  *
- *  Distributed under the terms of the GPL (GNU Public License)
+ *	*** BEGIN LICENSE INFORMATION ***
+ *	
+ *	The contents of this file are subject to the Common Public Attribution License 
+ *	Version 1.0 (the "License"); you may not use this file except in compliance with 
+ *	the License. You may obtain a copy of the License at 
+ *	http://lass.sourceforge.net/cpal-license. The License is based on the 
+ *	Mozilla Public License Version 1.1 but Sections 14 and 15 have been added to cover 
+ *	use of software over a computer network and provide for limited attribution for 
+ *	the Original Developer. In addition, Exhibit A has been modified to be consistent 
+ *	with Exhibit B.
+ *	
+ *	Software distributed under the License is distributed on an "AS IS" basis, WITHOUT 
+ *	WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+ *	language governing rights and limitations under the License.
+ *	
+ *	The Original Code is LASS - Library of Assembled Shared Sources.
+ *	
+ *	The Initial Developer of the Original Code is Bram de Greve and Tom De Muer.
+ *	The Original Developer is the Initial Developer.
+ *	
+ *	All portions of the code written by the Initial Developer are:
+ *	Copyright (C) 2004-2007 the Initial Developer.
+ *	All Rights Reserved.
+ *	
+ *	Contributor(s):
  *
- *  The LASS License:
- *
- *  Copyright 2004-2006 Bram de Greve and Tom De Muer
- *
- *  LASS is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *	Alternatively, the contents of this file may be used under the terms of the 
+ *	GNU General Public License Version 2 or later (the GPL), in which case the 
+ *	provisions of GPL are applicable instead of those above.  If you wish to allow use
+ *	of your version of this file only under the terms of the GPL and not to allow 
+ *	others to use your version of this file under the CPAL, indicate your decision by 
+ *	deleting the provisions above and replace them with the notice and other 
+ *	provisions required by the GPL License. If you do not delete the provisions above,
+ *	a recipient may use your version of this file under either the CPAL or the GPL.
+ *	
+ *	*** END LICENSE INFORMATION ***
  */
 
 #ifndef LASS_GUARDIAN_OF_INCLUSION_STDE_OVERWRITE_INSERT_ITERATOR_H
@@ -34,42 +51,62 @@ namespace stde
 {
 
 template <typename Container>
-class overwrite_insert_iterator_t: std::iterator<std::output_iterator, void, void, void, void>
+class overwrite_insert_iterator: std::iterator<std::output_iterator_tag, void, void, void, void>
 {
-	typedef Container container_type;
+public:
 
-	explicit overwrite_insert_iterator_t(container_type& container): 
+	typedef Container container_type;
+	typedef typename Container::iterator iterator;
+	typedef typename Container::const_iterator const_iterator;
+
+	explicit overwrite_insert_iterator(container_type& container): 
 		container_(container),
-		pos_(container.begin())
+		end_(container.begin())
 	{
 	}
 
-	overwrite_insert_iterator_t<Container>& operator=(typename Container::const_reference x)
+	overwrite_insert_iterator& operator=(const overwrite_insert_iterator& other)
 	{
-		if (pos_ != container.end())
+		container_ = other.container_;
+		end_ = other.end_;
+		return *this;
+	}
+
+	overwrite_insert_iterator& operator=(typename Container::const_reference x)
+	{
+		if (end_ != container_.end())
 		{
-			*pos_++ = x;
+			*end_++ = x;
 		}
 		else
 		{
-			container.push_back(x);
-			pos_ = container.end();
+			container_.push_back(x);
+			end_ = container_.end();
 		}
 		return *this;
 	}
 
-	overwrite_insert_iterator_t<Container>&} operator*() { return *this; }
-	overwrite_insert_iterator_t<Container>&} operator++() { return *this; }
-	overwrite_insert_iterator_t<Container>&} operator++(int) { return *this; }
+	overwrite_insert_iterator& operator*() { return *this; }
+	overwrite_insert_iterator& operator++() { return *this; }
+	overwrite_insert_iterator& operator++(int) { return *this; }
 
-	typename Container::iterator pos() const { return pos_; }
-	void pos(typename Container::iterator i) { pos_ = i; }
+	typename Container::iterator begin() { return container_.begin(); }
+	typename Container::iterator end() { return end_; }
+	typename Container::const_iterator begin() const { return container_.begin(); }
+	typename Container::const_iterator end() const { return end_; }
+	//void end(typename Container::iterator i) { end_ = i; }
 
 private:
 
-	Container& container;
-	typename Container::iterator pos_;
+	Container& container_;
+	typename Container::iterator end_;
 };
+
+template <typename Container>
+overwrite_insert_iterator<Container> overwrite_inserter(Container& container) 
+{
+	return overwrite_insert_iterator<Container>(container);
+}
 
 }
 }
