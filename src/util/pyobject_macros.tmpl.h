781f98667926f91e7327a6cc01a19362514b9d57
Tom De Muer <tom@cocamware.com>
2007-09-24 19:31:14 +0000

    * removing the ability to derive from embedded objects, this causes crashes without the __dict__ present, now a user will at least be notified that inheritance is not supported
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/trunk@691 e9412239-ebb7-49aa-8f4a-f9aa784616d7

--- src/util/pyobject_macros.tmpl.h
+++ src/util/pyobject_macros.tmpl.h
@@ -1969,7 +1969,7 @@ $[
 	0,/*PyObject_GenericGetAttr ,*/	/*tp_getattro */\
 	0,/*PyObject_GenericSetAttr,*/	/*tp_setattro */\
 	0,	/*tp_as_buffer*/\
-	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_CLASS ,	/*tp_flags*/\
+	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_CLASS ,	/*tp_flags*/\
 	0,	/*tp_doc*/\
 	0,	/*tp_traverse*/\
 	0,	/*tp_clear*/\
