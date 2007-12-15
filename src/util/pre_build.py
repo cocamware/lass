9bc0d4962f9e98a228c5acb16b7fff8e1dd2d9d6
Tom De Muer <tom@cocamware.com>
2007-12-15 12:55:45 +0000

    more arguments to templates
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/trunk@716 e9412239-ebb7-49aa-8f4a-f9aa784616d7

--- src/util/pre_build.py
+++ src/util/pre_build.py
@@ -2,24 +2,25 @@ import os.path
 execfile(os.path.join(os.pardir, os.pardir, 'tools', 'param_expander.py'))
 
 print 'Code generation ...'
+N = 15
 
-expandFile('object_factory.tmpl.h', 'object_factory.h', 10)
-expandFile('clone_factory.tmpl.h', 'clone_factory.h', 10)
+expandFile('object_factory.tmpl.h', 'object_factory.h', N)
+expandFile('clone_factory.tmpl.h', 'clone_factory.h', N)
 
-expandFile('callback.tmpl.h', 'callback.h', 10)
-expandFile('callback_x.tmpl.h', 'callback_$x.h', 10)
-expandFile('callback_r_x.tmpl.h', 'callback_r_$x.h', 10)
-expandFile('callback_python.tmpl.h', 'callback_python.h', 10)
-expandFile('callback_python.tmpl.inl', 'callback_python.inl', 10)
-expandFile('impl/dispatcher_x.tmpl.h', 'impl/dispatcher_$x.h', 10)
-expandFile('impl/dispatcher_r_x.tmpl.h', 'impl/dispatcher_r_$x.h', 10)
-expandFile('impl/dispatcher_python.tmpl.h', 'impl/dispatcher_python.h', 10)
+expandFile('callback.tmpl.h', 'callback.h', N)
+expandFile('callback_x.tmpl.h', 'callback_$x.h', N)
+expandFile('callback_r_x.tmpl.h', 'callback_r_$x.h', N)
+expandFile('callback_python.tmpl.h', 'callback_python.h', N)
+expandFile('callback_python.tmpl.inl', 'callback_python.inl', N)
+expandFile('impl/dispatcher_x.tmpl.h', 'impl/dispatcher_$x.h', N)
+expandFile('impl/dispatcher_r_x.tmpl.h', 'impl/dispatcher_r_$x.h', N)
+expandFile('impl/dispatcher_python.tmpl.h', 'impl/dispatcher_python.h', N)
 
-expandFile('pyobject_macros.tmpl.h', 'pyobject_macros.h', 10)
-expandFile('pyobject_call.tmpl.inl', 'pyobject_call.inl', 10)
+expandFile('pyobject_macros.tmpl.h', 'pyobject_macros.h', N)
+expandFile('pyobject_call.tmpl.inl', 'pyobject_call.inl', N)
 expandFile('py_tuple.tmpl.h', 'py_tuple.h', 20)
 
-expandFile('thread_fun.tmpl.h', 'thread_fun.h', 10)
-expandFile('thread_fun.tmpl.inl', 'thread_fun.inl', 10)
+expandFile('thread_fun.tmpl.h', 'thread_fun.h', N)
+expandFile('thread_fun.tmpl.inl', 'thread_fun.inl', N)
 
-expandFile('bind.tmpl.h', 'bind.h', 10)
+expandFile('bind.tmpl.h', 'bind.h', N)
