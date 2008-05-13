a7bc05c5b4ae9901115abd44384e8152a7bdc4fb
Bram de Greve <bram.degreve@bramz.net>
2008-05-13 14:34:36 +0000

    fixing weird configuration problems (hopefully)
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/trunk@754 e9412239-ebb7-49aa-8f4a-f9aa784616d7

--- build_vc8_dll.py
+++ build_vc8_dll.py
@@ -2,6 +2,6 @@ from tools.build_dll import build_dll
 
 solution_file = 'src/dll_vc8.sln'
 project_file = 'src/dll/dll_vc8.vcproj'
-configurations = ['vc8|Win32','vc8_d|Win32', 'vc8_sse2|Win32']
+configurations = ['"vc8|Win32"','"vc8_d|Win32"', '"vc8_sse2|Win32"']
 comntools = 'vs80comntools'
 build_dll(solution_file, project_file, configurations, comntools)
