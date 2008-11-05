133fe5bce32083c24ee455dd02216b0ce66035b7
Bram de Greve <bram.degreve@bramz.net>
2008-11-05 19:21:24 +0000

    moving CMakeLists.txt and CTestConfig.cmake to top level, deleting some dead files.
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/branches/cmake-experimentation@868 e9412239-ebb7-49aa-8f4a-f9aa784616d7

--- src/test/CTestConfig.cmake
+++
@@ -1,7 +0,0 @@
-set(CTEST_PROJECT_NAME "Lass")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
-
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "cdash.bramz.net")
-set(CTEST_DROP_LOCATION "/submit.php?project=Lass")
-set(CTEST_DROP_SITE_CDASH TRUE)
