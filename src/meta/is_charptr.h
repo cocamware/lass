1143a94d5dc6d2e892e0acf822f81d1fd3144da3
Tom De Muer <tom@cocamware.com>
2008-11-13 21:43:36 +0000

    tools to identify char strings at compile time
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/trunk@879 e9412239-ebb7-49aa-8f4a-f9aa784616d7

---
+++ src/meta/is_charptr.h
@@ -0,0 +1,89 @@
+/**	@file
+ *	@author Bram de Greve (bramz@users.sourceforge.net)
+ *	@author Tom De Muer (tomdemuer@users.sourceforge.net)
+ *
+ *	*** BEGIN LICENSE INFORMATION ***
+ *	
+ *	The contents of this file are subject to the Common Public Attribution License 
+ *	Version 1.0 (the "License"); you may not use this file except in compliance with 
+ *	the License. You may obtain a copy of the License at 
+ *	http://lass.sourceforge.net/cpal-license. The License is based on the 
+ *	Mozilla Public License Version 1.1 but Sections 14 and 15 have been added to cover 
+ *	use of software over a computer network and provide for limited attribution for 
+ *	the Original Developer. In addition, Exhibit A has been modified to be consistent 
+ *	with Exhibit B.
+ *	
+ *	Software distributed under the License is distributed on an "AS IS" basis, WITHOUT 
+ *	WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+ *	language governing rights and limitations under the License.
+ *	
+ *	The Original Code is LASS - Library of Assembled Shared Sources.
+ *	
+ *	The Initial Developer of the Original Code is Bram de Greve and Tom De Muer.
+ *	The Original Developer is the Initial Developer.
+ *	
+ *	All portions of the code written by the Initial Developer are:
+ *	Copyright (C) 2004-2007 the Initial Developer.
+ *	All Rights Reserved.
+ *	
+ *	Contributor(s):
+ *
+ *	Alternatively, the contents of this file may be used under the terms of the 
+ *	GNU General Public License Version 2 or later (the GPL), in which case the 
+ *	provisions of GPL are applicable instead of those above.  If you wish to allow use
+ *	of your version of this file only under the terms of the GPL and not to allow 
+ *	others to use your version of this file under the CPAL, indicate your decision by 
+ *	deleting the provisions above and replace them with the notice and other 
+ *	provisions required by the GPL License. If you do not delete the provisions above,
+ *	a recipient may use your version of this file under either the CPAL or the GPL.
+ *	
+ *	*** END LICENSE INFORMATION ***
+ */
+
+#ifndef LASS_GUARDIAN_OF_INCLUSION_META_IS_CONVERTIBLE_H
+#define LASS_GUARDIAN_OF_INCLUSION_META_IS_CONVERTIBLE_H
+
+#include "meta_common.h"
+#include "bool.h"
+
+#if LASS_COMPILER_TYPE == LASS_COMPILER_TYPE_MSVC
+#	pragma warning(push)
+#	pragma warning(disable: 4244) // conversion from 'foo' to 'bar': possible loss of data
+#endif
+
+namespace lass
+{
+namespace meta
+{
+namespace impl
+{
+struct IsCharPtrTester
+{
+	static lass::meta::True test(char *);
+	static lass::meta::True test(const char *);
+	static lass::meta::False test(...);
+};
+template<int value>
+struct IsCharPtrValue : public lass::meta::False
+{
+};
+template<>
+struct IsCharPtrValue< sizeof(IsCharPtrTester::test("Ed's Super Fixit")) > : public lass::meta::True
+{
+};
+}
+
+#define IsCharPtr( x ) (lass::meta::impl::IsCharPtrValue<sizeof(lass::meta::impl::IsCharPtrTester::test(x))>::value)
+#define IsCharPtrType( x ) lass::meta::impl::IsCharPtrValue<sizeof(lass::meta::impl::IsCharPtrTester::test(x))>::Type
+
+
+}
+}
+
+#if LASS_COMPILER_TYPE == LASS_COMPILER_TYPE_MSVC
+#	pragma warning(pop)
+#endif
+
+#endif
+
+// EOF
