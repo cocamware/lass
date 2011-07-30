952ebe9b5c49a9c5b2ccc50f1ecd74e33304d1f6
Bram de Greve <bram.degreve@bramz.net>
2011-07-30 15:24:18 +0000

    wrapper around LoadLibrary
    git-svn-id: https://svn.code.sf.net/p/lass/code/lass/trunk@1128 e9412239-ebb7-49aa-8f4a-f9aa784616d7

---
+++ lass/util/dynamic_library.cpp
@@ -0,0 +1,107 @@
+/**	@file
+ *	@author Bram de Greve (bram@cocamware.com)
+ *	@author Tom De Muer (tom@cocamware.com)
+ *
+ *	*** BEGIN LICENSE INFORMATION ***
+ *	
+ *	The contents of this file are subject to the Common Public Attribution License 
+ *	Version 1.0 (the "License"); you may not use this file except in compliance with 
+ *	the License. You may obtain a copy of the License at 
+ *	http://lass.sourceforge.net/cpal-license. The License is based on the 
+ *	Mozilla Public License Version 1.1 but Sections 14 and 15 have been added to cover 
+ *	use of software over a computer network and provide for limited attribution for 
+ *	the Original Developer. In addition, Exhibit A has been modified to be consistent 
+ *	with Exhibit B.
+ *	
+ *	Software distributed under the License is distributed on an "AS IS" basis, WITHOUT 
+ *	WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+ *	language governing rights and limitations under the License.
+ *	
+ *	The Original Code is LASS - Library of Assembled Shared Sources.
+ *	
+ *	The Initial Developer of the Original Code is Bram de Greve and Tom De Muer.
+ *	The Original Developer is the Initial Developer.
+ *	
+ *	All portions of the code written by the Initial Developer are:
+ *	Copyright (C) 2004-2010 the Initial Developer.
+ *	All Rights Reserved.
+ *	
+ *	Contributor(s):
+ *
+ *	Alternatively, the contents of this file may be used under the terms of the 
+ *	GNU General Public License Version 2 or later (the GPL), in which case the 
+ *	provisions of GPL are applicable instead of those above.  If you wish to allow use
+ *	of your version of this file only under the terms of the GPL and not to allow 
+ *	others to use your version of this file under the CPAL, indicate your decision by 
+ *	deleting the provisions above and replace them with the notice and other 
+ *	provisions required by the GPL License. If you do not delete the provisions above,
+ *	a recipient may use your version of this file under either the CPAL or the GPL.
+ *	
+ *	*** END LICENSE INFORMATION ***
+ */
+
+#include "lass_common.h"
+#include "dynamic_library.h"
+#include "impl/lass_errno.h"
+#include "../stde/extended_string.h"
+
+#if LASS_HAVE_WINDOWS_H
+
+#include <windows.h>
+
+namespace
+{
+	struct DynamicLibraryHandle
+	{
+		HMODULE module;
+	};
+}
+
+namespace lass
+{
+namespace util
+{
+
+DynamicLibrary::DynamicLibrary(const std::string& path):
+	pimpl_(0)
+{
+	pimpl_ = ::LoadLibrary(path.c_str());
+	if (pimpl_ == NULL && !stde::ends_with(path, ".dll") && !stde::ends_with(path, ".pyd"))
+	{
+		pimpl_ = ::LoadLibrary( (path + ".dll").c_str() );
+		if (pimpl_ == NULL)
+		{
+			pimpl_ = ::LoadLibrary( (path + ".pyd").c_str() );
+		}
+	}
+	if (pimpl_ == NULL)
+	{
+		LASS_THROW_EX(DynamicLibraryError, "Unable to load dynamic library '" << path << "': " << impl::lass_FormatMessage(impl::lass_GetLastError()));
+	}
+}
+
+DynamicLibrary::~DynamicLibrary()
+{
+	LASS_ASSERT(pimpl_);
+	::FreeLibrary(static_cast<HMODULE>(pimpl_));
+}
+
+DynamicLibrary::TFunctionPtr DynamicLibrary::getFunctionImpl(const std::string& functionName) const
+{
+	LASS_ASSERT(pimpl_);
+	TFunctionPtr fun = TFunctionPtr(::GetProcAddress(static_cast<HMODULE>(pimpl_), functionName.c_str()));
+	if (!fun)
+	{
+		LASS_THROW_EX(DynamicLibraryError, "Unable to find " << functionName);
+	}
+	return fun;
+}
+
+}
+}
+
+#else
+
+#endif
+
+// EOF
---
+++ lass/util/dynamic_library.h
@@ -0,0 +1,86 @@
+/**	@file
+ *	@author Bram de Greve (bram@cocamware.com)
+ *	@author Tom De Muer (tom@cocamware.com)
+ *
+ *	*** BEGIN LICENSE INFORMATION ***
+ *	
+ *	The contents of this file are subject to the Common Public Attribution License 
+ *	Version 1.0 (the "License"); you may not use this file except in compliance with 
+ *	the License. You may obtain a copy of the License at 
+ *	http://lass.sourceforge.net/cpal-license. The License is based on the 
+ *	Mozilla Public License Version 1.1 but Sections 14 and 15 have been added to cover 
+ *	use of software over a computer network and provide for limited attribution for 
+ *	the Original Developer. In addition, Exhibit A has been modified to be consistent 
+ *	with Exhibit B.
+ *	
+ *	Software distributed under the License is distributed on an "AS IS" basis, WITHOUT 
+ *	WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+ *	language governing rights and limitations under the License.
+ *	
+ *	The Original Code is LASS - Library of Assembled Shared Sources.
+ *	
+ *	The Initial Developer of the Original Code is Bram de Greve and Tom De Muer.
+ *	The Original Developer is the Initial Developer.
+ *	
+ *	All portions of the code written by the Initial Developer are:
+ *	Copyright (C) 2004-2011 the Initial Developer.
+ *	All Rights Reserved.
+ *	
+ *	Contributor(s):
+ *
+ *	Alternatively, the contents of this file may be used under the terms of the 
+ *	GNU General Public License Version 2 or later (the GPL), in which case the 
+ *	provisions of GPL are applicable instead of those above.  If you wish to allow use
+ *	of your version of this file only under the terms of the GPL and not to allow 
+ *	others to use your version of this file under the CPAL, indicate your decision by 
+ *	deleting the provisions above and replace them with the notice and other 
+ *	provisions required by the GPL License. If you do not delete the provisions above,
+ *	a recipient may use your version of this file under either the CPAL or the GPL.
+ *	
+ *	*** END LICENSE INFORMATION ***
+ */
+
+#ifndef LASS_GUARDIAN_OF_INCLUSION_UTIL_DYNAMIC_LIBRARY_H
+#define LASS_GUARDIAN_OF_INCLUSION_UTIL_DYNAMIC_LIBRARY_H
+
+#include "util_common.h"
+
+namespace lass
+{
+namespace util
+{
+
+class LASS_DLL DynamicLibrary: NonCopyable
+{
+public:
+
+	DynamicLibrary(const std::string& path);
+	~DynamicLibrary();
+
+	template <typename FunPtr>
+	FunPtr getFunction(const std::string& symbol) const
+	{
+		return static_cast<FunPtr>(getFunctionImpl(symbol));
+	}
+
+private:
+	typedef int (*TFunctionPtr)(void);
+	TFunctionPtr getFunctionImpl (const std::string& symbol) const;
+	void* pimpl_;
+};
+
+
+
+class DynamicLibraryError: public ExceptionMixin<DynamicLibraryError>
+{
+public:
+	DynamicLibraryError(const std::string& msg, const std::string& loc): ExceptionMixin<DynamicLibraryError>(msg, loc) {}
+	~DynamicLibraryError() throw() {}
+};
+
+}
+}
+
+#endif
+
+// EOF
