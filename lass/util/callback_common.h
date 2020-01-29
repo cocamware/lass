cocamware/lass

commit 7a3e5cd2b660b0fe9868ea39a9c862f9200ecaad

Support callables without operator! in Callback
With C++11, lambdas can also be used as callables. But the MSVC compiler
did not support the unary ! operator. This was fixed in VS 2019 version
16.2. See https://twitter.com/lunasorcery/status/1092870113374687232

This specialisation relies on SFINAE techniques. First, there's
HasOperatorNot that detects if a type supports operator! using some
sizeof trickery (sizeof(test<T>(0)) can be computed by only substituting
types). Secondly, EnableIf will only have Type if
!HasOperatorNot<F>::value is true, so that in the other case, the
specialization will not be considered.

lass/util/callback_common.h
@@ -23,7 +23,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2011 the Initial Developer.
+ *	Copyright (C) 2004-2020 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
@@ -58,6 +58,20 @@ public:
 	EmptyCallback(const std::string& msg, const std::string& loc): ExceptionMixin<EmptyCallback>(msg, loc) {}
 	~EmptyCallback() throw() {}
 };
+
+
+namespace impl
+{
+
+template <typename T>
+class HasOperatorNot {
+	template <typename C> static meta::True test(char[!C()]);
+	template <typename C> static meta::False test(...);
+public:
+	enum { value = sizeof(test<T>(0)) == sizeof(meta::True) };
+};
+
+}
 
 }
 }

lass/meta/enable_if.h
@@ -1,0 +1,67 @@
+/**	@file
+ *	@author Bram de Greve (bram@cocamware.com)
+ *	@author Tom De Muer (tom@cocamware.com)
+ *
+ *	*** BEGIN LICENSE INFORMATION ***
+ *
+ *	The contents of this file are subject to the Common Public Attribution License
+ *	Version 1.0 (the "License"); you may not use this file except in compliance with
+ *	the License. You may obtain a copy of the License at
+ *	http://lass.sourceforge.net/cpal-license. The License is based on the
+ *	Mozilla Public License Version 1.1 but Sections 14 and 15 have been added to cover
+ *	use of software over a computer network and provide for limited attribution for
+ *	the Original Developer. In addition, Exhibit A has been modified to be consistent
+ *	with Exhibit B.
+ *
+ *	Software distributed under the License is distributed on an "AS IS" basis, WITHOUT
+ *	WARRANTY OF ANY KIND, either express or implied. See the License for the specific
+ *	language governing rights and limitations under the License.
+ *
+ *	The Original Code is LASS - Library of Assembled Shared Sources.
+ *
+ *	The Initial Developer of the Original Code is Bram de Greve and Tom De Muer.
+ *	The Original Developer is the Initial Developer.
+ *
+ *	All portions of the code written by the Initial Developer are:
+ *	Copyright (C) 2004-2020 the Initial Developer.
+ *	All Rights Reserved.
+ *
+ *	Contributor(s):
+ *
+ *	Alternatively, the contents of this file may be used under the terms of the
+ *	GNU General Public License Version 2 or later (the GPL), in which case the
+ *	provisions of GPL are applicable instead of those above.  If you wish to allow use
+ *	of your version of this file only under the terms of the GPL and not to allow
+ *	others to use your version of this file under the CPAL, indicate your decision by
+ *	deleting the provisions above and replace them with the notice and other
+ *	provisions required by the GPL License. If you do not delete the provisions above,
+ *	a recipient may use your version of this file under either the CPAL or the GPL.
+ *
+ *	*** END LICENSE INFORMATION ***
+ */
+
+#ifndef LASS_GUADIAN_OF_INCLUSION_META_ENABLE_IF_H
+#define LASS_GUADIAN_OF_INCLUSION_META_ENABLE_IF_H
+
+#include "meta_common.h"
+
+namespace lass
+{
+namespace meta
+{
+
+template<bool B, class T = void>
+struct EnableIf {};
+
+template<class T>
+struct EnableIf<true, T>
+{
+	typedef T Type;
+};
+
+}
+}
+
+#endif
+
+// EOF

lass/util/impl/dispatcher_0.h
@@ -23,7 +23,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2011 the Initial Developer.
+ *	Copyright (C) 2004-2020 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
@@ -44,6 +44,7 @@
 #define LASS_GUARDIAN_OF_INCLUSION_UTIL_IMPL_DISPATCHER_0_H
 
 #include "dispatcher_allocator.h"
+#include "../../meta/enable_if.h"
 
 // --- NEW INTERFACES ----------------------------------------------------------
 
@@ -95,13 +96,14 @@ private:
  */
 template 
 <
-	typename Function
+	typename Function,
+	typename Enable = void
 >
 class Dispatcher0Function: public Dispatcher0
 {
 public:
 
-	typedef Dispatcher0Function<Function> TSelf;
+	typedef Dispatcher0Function<Function, Enable> TSelf;
 	typedef Function TFunction;
 
 	Dispatcher0Function(typename CallTraits<TFunction>::TParam iFunction):
@@ -123,6 +125,47 @@ private:
 	{
 		const TSelf* other = dynamic_cast<const TSelf*>(iOther);
 		return other && function_ == other->function_;
+	}
+
+	TFunction function_;
+};
+
+
+
+/** Dispatcher for lass::util::Callback0 to a callable that does not support operator!
+ *  @internal
+ *  @sa Callback0
+ *  @author Bram de Greve [Bramz]
+ * 
+ *  With C++11, lambdas can also be used as callables. But the MSVC compiler did not support
+ *  the unary ! operator. This was fixed in VS 2019 version 16.2.
+ *  https://twitter.com/lunasorcery/status/1092870113374687232
+ */
+template
+<
+	typename Function
+>
+class Dispatcher0Function< Function, typename meta::EnableIf<!HasOperatorNot<Function>::value>::Type >: public Dispatcher0
+{
+public:
+
+	typedef Dispatcher0Function<Function, typename meta::EnableIf<!HasOperatorNot<Function>::value>::Type > TSelf;
+	typedef Function TFunction;
+
+	Dispatcher0Function(typename CallTraits<TFunction>::TParam iFunction):
+		function_(iFunction)
+	{
+	}
+
+private:
+
+	void doCall() const
+	{
+		function_();
+	}
+	bool doIsEquivalent(const Dispatcher0* /*iOther*/) const
+	{
+		return false;
 	}
 
 	TFunction function_;

lass/util/impl/dispatcher_r_0.h
@@ -23,7 +23,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2011 the Initial Developer.
+ *	Copyright (C) 2004-2020 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
@@ -96,7 +96,8 @@ private:
 template
 <
 	typename R,
-	typename FunctionType
+	typename FunctionType,
+	typename Enable = void
 >
 class DispatcherR0Function: public DispatcherR0<R>
 {
@@ -123,6 +124,48 @@ private:
 	TFunction function_;
 };
 
+
+
+/** Dispatcher for lass::util::CallbackR0 to a callable that does not support operator!
+ *  @internal
+ *  @sa CallbackR0
+ *  @author Bram de Greve [Bramz]
+ * 
+ *  With C++11, lambdas can also be used as callables. But the MSVC compiler did not support
+ *  the unary ! operator. This was fixed in VS 2019 version 16.2.
+ *  https://twitter.com/lunasorcery/status/1092870113374687232
+ */
+template
+<
+	typename R,
+	typename FunctionType
+>
+class DispatcherR0Function
+	<
+		R,
+		FunctionType,
+		typename meta::EnableIf<!HasOperatorNot<FunctionType>::value>::Type
+	>
+	: public DispatcherR0<R>
+{
+public:
+
+	typedef FunctionType TFunction;
+
+	DispatcherR0Function(typename CallTraits<TFunction>::TParam iFunction):
+		function_(iFunction)
+	{
+	}
+
+private:
+
+	R doCall() const
+	{
+		return function_();
+	}
+
+	TFunction function_;
+};
 
 
 /** Dispatcher for lass::util::CallbackR0 to an object/method pair.

lass/util/impl/dispatcher_r_x.tmpl.h
@@ -23,7 +23,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2011 the Initial Developer.
+ *	Copyright (C) 2004-2020 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
@@ -94,7 +94,8 @@ private:
 template
 <
 	typename R, $(typename P$x)$,
-	typename FunctionType
+	typename FunctionType,
+	typename Enable = void
 >
 class DispatcherR$xFunction: public DispatcherR$x<R, $(P$x)$>
 {
@@ -115,6 +116,49 @@ private:
 		{
 			LASS_THROW_EX(EmptyCallback, "You've tried to call an empty CallbackR0.  Can't return a value.");
 		}
+		return function_($(iP$x)$);
+	}
+
+	TFunction function_;
+};
+
+
+
+/** Dispatcher for lass::util::CallbackR0 to a callable that does not support operator!
+ *  @internal
+ *  @sa CallbackR0
+ *  @author Bram de Greve [Bramz]
+ * 
+ *  With C++11, lambdas can also be used as callables. But the MSVC compiler did not support
+ *  the unary ! operator. This was fixed in VS 2019 version 16.2.
+ *  https://twitter.com/lunasorcery/status/1092870113374687232
+ */
+template
+<
+	typename R, $(typename P$x)$,
+	typename FunctionType
+>
+class DispatcherR$xFunction
+	<
+		R, $(P$x)$,
+		FunctionType,
+		typename meta::EnableIf<!HasOperatorNot<FunctionType>::value>::Type
+	>
+	: public DispatcherR$x<R, $(P$x)$>
+{
+public:
+
+	typedef FunctionType TFunction;
+
+	DispatcherR$xFunction(typename CallTraits<TFunction>::TParam iFunction):
+		function_(iFunction)
+	{
+	}
+
+private:
+
+	R doCall($(typename util::CallTraits<P$x>::TParam iP$x)$) const
+	{
 		return function_($(iP$x)$);
 	}
 

lass/util/impl/dispatcher_x.tmpl.h
@@ -23,7 +23,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2011 the Initial Developer.
+ *	Copyright (C) 2004-2020 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
@@ -46,6 +46,7 @@
 #define LASS_GUARDIAN_OF_INCLUSION_UTIL_IMPL_DISPATCHER_$x_H
 
 #include "dispatcher_allocator.h"
+#include "../../meta/enable_if.h"
 
 // --- NEW INTERFACES ----------------------------------------------------------
 
@@ -95,7 +96,8 @@ private:
 template
 <
 	$(typename P$x)$, 
-	typename FunctionType
+	typename FunctionType,
+	typename Enable = void
 >
 class Dispatcher$xFunction: public Dispatcher$x<$(P$x)$>
 {
@@ -121,6 +123,50 @@ private:
 
 	TFunction function_;
 };
+
+
+
+/** Dispatcher for lass::util::Callback$x to a callable that does not support operator!
+ *  @internal
+ *  @sa Callback0
+ *  @author Bram de Greve [Bramz]
+ * 
+ *  With C++11, lambdas can also be used as callables. But the MSVC compiler did not support
+ *  the unary ! operator. This was fixed in VS 2019 version 16.2.
+ *  https://twitter.com/lunasorcery/status/1092870113374687232
+ */
+template
+<
+	$(typename P$x)$,
+	typename FunctionType
+>
+class Dispatcher$xFunction
+	<
+		$(P$x)$,
+		FunctionType,
+		typename meta::EnableIf<!HasOperatorNot<FunctionType>::value>::Type
+	>
+	: public Dispatcher$x<$(P$x)$>
+{
+public:
+
+	typedef FunctionType TFunction;
+
+	Dispatcher$xFunction(typename CallTraits<TFunction>::TParam iFunction):
+		function_(iFunction)
+	{
+	}
+
+private:
+
+	void doCall($(typename util::CallTraits<P$x>::TParam iP$x)$) const
+	{
+		function_($(iP$x)$);
+	}
+
+	TFunction function_;
+};
+
 
 
 

test_suite/test_util_callback.cpp.in
@@ -23,7 +23,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2007 the Initial Developer.
+ *	Copyright (C) 2004-2020 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
@@ -235,6 +235,12 @@ void testUtilCallbackR0()
 	LASS_TEST_CALLBACK_NOT_EMPTY(cbr0_int_void_free);
 	LASS_TEST_CALLBACK_CALL_R(cbr0_int_void_free());
 
+#if LASS_HAVE_CPP_LAMBDAS
+	util::CallbackR0<int> cbr0_int_void_lambda([]() { return freeR0(); });
+	LASS_TEST_CALLBACK_NOT_EMPTY(cbr0_int_void_lambda);
+	LASS_TEST_CALLBACK_CALL_R(cbr0_int_void_lambda());
+#endif
+
 	//util::CallbackR0<float> cbr0_float_void_free(freeR0);
 	//LASS_TEST_CALLBACK_NOT_EMPTY(cbr0_float_void_free);
 	//LASS_TEST_CALLBACK_CALL_R(cbr0_float_void_free());
@@ -287,6 +293,12 @@ void testUtilCallbackR$x()
 	util::CallbackR$x<int, $(int)$> cbr$x_int_int_free(freeR$x);
 	LASS_TEST_CALLBACK_NOT_EMPTY(cbr$x_int_int_free);
 	LASS_TEST_CALLBACK_CALL_R(cbr$x_int_int_free($($x)$));
+
+#if LASS_HAVE_CPP_LAMBDAS
+	util::CallbackR$x<int, $(int)$> cbr$x_int_int_lambda([]($(int p$x)$) { return freeR$x($(p$x)$); });
+	LASS_TEST_CALLBACK_NOT_EMPTY(cbr$x_int_int_lambda);
+	LASS_TEST_CALLBACK_CALL_R(cbr$x_int_int_lambda($($x)$));
+#endif
 
 	//util::CallbackR$x<float, $(float)$> cbr$x_float_float_free(freeR$x);
 	//LASS_TEST_CALLBACK_NOT_EMPTY(cbr$x_float_float_free);

commit f740e3c8c2ccf3575f8b83ff8c6dffea9b54d866

Include missing header
This fixes a regression introduced in commit
7a3e5cd2b660b0fe9868ea39a9c862f9200ecaad

lass/util/callback_common.h
@@ -44,6 +44,7 @@
 #define LASS_GUARDIAN_OF_INCLUSION_UTIL_CALLBACK_COMMON_H
 
 #include "util_common.h"
+#include "../meta/enable_if.h"
 
 namespace lass
 {