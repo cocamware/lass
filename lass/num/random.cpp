63ea3ba26e8e1666e8680bfa9e99ce169d6976a2
Bram de Greve <bram.degreve@bramz.net>
2022-11-21 19:19:34 +0100

    Fix RandomParkMiller::max
    The maximum returned value is one less than the modulus.

--- lass/num/random.cpp
+++ lass/num/random.cpp
@@ -23,7 +23,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2011 the Initial Developer.
+ *	Copyright (C) 2004-2022 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
@@ -59,7 +59,7 @@ const RandomStandard::TValue RandomStandard::max = RAND_MAX;
 
 // --- RandomParkMiller ----------------------------------------------------------------------------
 
-const RandomParkMiller::TValue RandomParkMiller::max = RandomParkMiller::modulus_;
+const RandomParkMiller::TValue RandomParkMiller::max = RandomParkMiller::modulus_ - 1;
 
 
 
