bcb6816bb393bc2dea0dfd881ce9fc32ac3216c7
Bram de Greve <bram.degreve@bramz.net>
2023-07-02 20:01:49 +0200

    Add bidirectional export traits for std::function
    One particular bug in lass_stubgen was in parsing the parameters of
    functions that return a std::function<R(T, U)> (with R, T, U actual
    types like int, not template paramters): the parameters T and U of the
    _return_ type would also be counted as parameters of the function.
    That's because in libclang's AST the components of the function
    signature gets flattend and we can't readily distinguish between
    PARAM_DECL of actual parameters and template arguments:
    
      FUNCTION_DECL testFunctionFromCpp type=std::function<int (int, int)> (int)
        NAMESPACE_REF std type=
        TEMPLATE_REF function type=
        PARM_DECL  type=int   ← 1st parameter of the function in return type
        PARM_DECL  type=int   ← 2nd parameter of the function in return type
        PARM_DECL factor type=int <- Actual parameter of testFunctionFromCpp
    
    In this case, only the actual parameter has a name ("factor"), but we
    can't rely on this in general. The most reliable check that I've found
    was that only the actual parameters have the function declaration as
    semantic parent.

--- tools/stubgen/lass_stubgen/generator.py
+++ tools/stubgen/lass_stubgen/generator.py
@@ -469,6 +469,7 @@ class StubGenerator:
 
         if cpp_name.endswith("*"):
             # pointer to a type, strip the pointer
+            assert cpp_type.result is None, cpp_type
             cpp_type_ = TypeInfo(cpp_name[:-1].rstrip(), cpp_type.args)
             return f"{self.python_type(cpp_type_, scope=scope)} | None"
 
@@ -568,7 +569,7 @@ def _match_template(
         return False  # one is None, the other is not
     if tmpl.name in matched_params:
         # it's a template parameter
-        assert tmpl.args is None
+        assert tmpl.args is None and tmpl.result is None
         if matched_params[tmpl.name] is None:
             matched_params[tmpl.name] = type_
         elif matched_params[tmpl.name] != type_:
@@ -577,6 +578,8 @@ def _match_template(
 
     # it's not a template parameter, so it need to match exactly
     if type_.name != tmpl.name:
+        return False
+    if not _match_template(type_.result, tmpl.result, matched_params):
         return False
     return _match_template_args(type_.args, tmpl.args, matched_params)
 
