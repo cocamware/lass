8c393db19923b32dd5b63b83c5d1e6dafbf28fc2
Bram de Greve <bram.degreve@bramz.net>
2025-10-08 22:21:58 +0200

    Always update copyright date in lass_common.h
    lass_common.h contains the mainpage for doxygen, mentioning the
    copyright notice.

--- lass/lass_common.h
+++ lass/lass_common.h
@@ -23,7 +23,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2011 the Initial Developer.
+ *	Copyright (C) 2004-2025 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
@@ -84,7 +84,7 @@
  *	The Original Developer is the Initial Developer.
  *	
  *	All portions of the code written by the Initial Developer are:
- *	Copyright (C) 2004-2011 the Initial Developer.
+ *	Copyright (C) 2004-2025 the Initial Developer.
  *	All Rights Reserved.
  *	
  *	Contributor(s):
