1319e48a8825e7ee2f5716c4c358e6ba2fc61cca
Bram de Greve <bram.degreve@bramz.net>
2025-10-08 22:25:45 +0200

    Release lass-1.14.0

--- setup.py
+++ setup.py
@@ -279,7 +279,7 @@ def get_version():
     # NOTE: you can only update this base tag _at_the_same_time_ you update
     # project(Lass VERSION ...) in CMakeLists. Otherwise, the prerelease numbers will
     # not properly increase.
-    base_tag = "lass-1.12.0"
+    base_tag = "lass-1.14.0"
 
     describe = subprocess.check_output(
         ["git", "describe", "--dirty=.dirty", "--tags", "--match", base_tag],
